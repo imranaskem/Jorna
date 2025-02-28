40e63c5a0ba7fd15af07b2675000426507bb08bc
Imran Askem <imran.askem@napo.pet>
2025-02-28 18:01:46 +0000

    Initial Commit

---
+++ Jorna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jorna
+//
+//  Created by Imran Askem on 28/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jorna/JornaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JornaApp.swift
+//  Jorna
+//
+//  Created by Imran Askem on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct JornaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
